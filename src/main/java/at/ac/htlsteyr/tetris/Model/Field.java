2db629ff03ae16a2efae5a379ef05d9ceb672fc5
Neuery17Alt <jangerer@htl-steyr.ac.at>
2023-03-07 15:08:26 +0100

    Update Field and Grid Class

--- src/main/java/at/ac/htlsteyr/tetris/Model/Field.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Field.java
@@ -1,14 +1,29 @@
 package at.ac.htlsteyr.tetris.Model;
 
+import javafx.scene.Node;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 
-public class Field {
+public class Field extends StackPane {
     private Rectangle fieldNode;
     private boolean containsBlock;
     private int xPosition;
     private int yPosition;
+    private Grid grid;
+    private final int FIELD_SIZE = 30;
 
-    public Field(Grid grid, int x, int y) {
+    public Field(int x, int y) {
+        this.xPosition = x;
+        this.yPosition = y;
+        fieldNode = new Rectangle(FIELD_SIZE, FIELD_SIZE);
+        fieldNode.setStroke(Color.RED);
 
+        getChildren().addAll(fieldNode);
+        setTranslateX(x*FIELD_SIZE);
+        setTranslateY(y*FIELD_SIZE);
     }
+
+
+
 }
--- src/main/java/at/ac/htlsteyr/tetris/Model/Grid.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Grid.java
@@ -1,6 +1,27 @@
 package at.ac.htlsteyr.tetris.Model;
 
-import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.Pane;
 
 public class Grid {
+    private Field[][] grid;
+    Pane root;
+
+    public Grid (Pane root) {
+        this.root = root;
+    }
+
+    public void generateGrid () {
+        root.setPrefSize(GridSize.width, GridSize.height);
+        int cols = GridSize.columns;
+        int rows = GridSize.rows;
+        grid = new Field[cols][rows];
+
+        for (int x = 0; x < cols; x++) {
+            for (int y = 0; y < rows; y++) {
+                Field field = new Field(cols, rows);
+                grid[x][y] = field;
+                root.getChildren().add(field);
+            }
+        }
+    }
 }
