5af697f2405cd2a45a98480337716b5e5f478748
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-03-12 21:27:11 +0100

    added tetromino grid

--- src/main/java/at/ac/htlsteyr/tetris/Model/Tetromino.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Tetromino.java
@@ -3,6 +3,14 @@ package at.ac.htlsteyr.tetris.Model;
 import java.util.ArrayList;
 
 public class Tetromino {
+
+    int[][] tetroGrid = {
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0},
+            {0, 0, 0, 0}
+    };
+
     public Tetromino(ArrayList<Block> blockList) {
 
     }
