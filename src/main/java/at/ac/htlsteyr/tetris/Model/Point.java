798e6b1f646c0a83533e44d1ddb6f2f30c818b75
Neuery17Alt <jangerer@htl-steyr.ac.at>
2023-03-13 11:31:32 +0100

    Update Point, add attributes for coordinates and added Getter+Setter

--- src/main/java/at/ac/htlsteyr/tetris/Model/Point.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Point.java
@@ -1,4 +1,21 @@
 package at.ac.htlsteyr.tetris.Model;
 
 public class Point {
+    int x,y;
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
 }
