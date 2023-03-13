3a28c0ca370c6b5ab988f883d96a5252ab271bab
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-03-13 22:08:14 +0100

    added constructor, seperated declaration of x and y

--- src/main/java/at/ac/htlsteyr/tetris/Model/Point.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Point.java
@@ -1,7 +1,13 @@
 package at.ac.htlsteyr.tetris.Model;
 
 public class Point {
-    int x,y;
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
 
     public int getX() {
         return x;
