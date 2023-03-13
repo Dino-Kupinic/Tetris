bc04e464fec4036f3c519be284fd750297b57eb4
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-03-13 21:32:43 +0100

    small improvements

--- src/main/java/at/ac/htlsteyr/tetris/Model/TetrominoFactory.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/TetrominoFactory.java
@@ -1,78 +1,78 @@
 package at.ac.htlsteyr.tetris.Model;
 
+import at.ac.htlsteyr.tetris.Exceptions.InvalidShapeException;
 import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
 
 public class TetrominoFactory {
-    public Tetromino createTetromino(Shapes shape) {
-        ArrayList<Block> arr = null;
-        Color col;
+    public Tetromino createTetromino(Shapes shape) throws InvalidShapeException {
         switch (shape) {
             case SHAPE_I -> {
-                col = Color.BLUE;
-                arr = new ArrayList<Block>(
+                Color col = Color.BLUE;
+                return new Tetromino(new ArrayList<Block>(
                         new Block(new Point(0, 1), col),
                         new Block(new Point(1, 1), col),
                         new Block(new Point(1, 2), col),
                         new Block(new Point(1, 3), col)
-                );
+                ));
             }
             case SHAPE_L -> {
-                col = Color.ORANGE;
-                arr = new ArrayList<Block>(
+                Color col = Color.ORANGE;
+                return new Tetromino(new ArrayList<Block>(
                         new Block(new Point(1, 1), col),
                         new Block(new Point(1, 2), col),
                         new Block(new Point(1, 3), col),
                         new Block(new Point(2, 3), col)
-                );
+                ));
             }
             case SHAPE_J -> {
-                col = Color.DARKBLUE;
-                arr = new ArrayList<Block>(
+                Color col = Color.DARKBLUE;
+                return new Tetromino(new ArrayList<Block>(
                         new Block(new Point(1, 1), col),
                         new Block(new Point(1, 2), col),
                         new Block(new Point(1, 3), col),
                         new Block(new Point(0, 3), col)
-                );
+                ));
             }
             case SHAPE_T -> {
-                col = Color.PURPLE;
-                arr = new ArrayList<Block>(
+                Color col = Color.PURPLE;
+                return new Tetromino(new ArrayList<Block>(
                         new Block(new Point(1, 2), col),
                         new Block(new Point(1, 3), col),
                         new Block(new Point(0, 3), col),
                         new Block(new Point(2, 3), col)
-                );
+                ));
             }
             case SHAPE_O -> {
-                col = Color.YELLOW;
-                arr = new ArrayList<Block>(
+                Color col = Color.YELLOW;
+                return new Tetromino(new ArrayList<Block>(
                         new Block(new Point(1, 2), col),
                         new Block(new Point(0, 2), col),
                         new Block(new Point(1, 3), col),
                         new Block(new Point(0, 3), col)
-                );
+                ));
             }
             case SHAPE_S -> {
-                col = Color.GREEN;
-                arr = new ArrayList<Block>(
+                Color col = Color.GREEN;
+                return new Tetromino(new ArrayList<Block>(
                         new Block(new Point(0, 3), col),
                         new Block(new Point(1, 3), col),
                         new Block(new Point(1, 2), col),
                         new Block(new Point(2, 2), col)
-                );
+                ));
             }
             case SHAPE_Z -> {
-                col = Color.RED;
-                arr = new ArrayList<Block>(
+                Color col = Color.RED;
+                return new Tetromino(new ArrayList<Block>(
                         new Block(new Point(0, 2), col),
                         new Block(new Point(1, 2), col),
                         new Block(new Point(1, 3), col),
                         new Block(new Point(2, 3), col)
-                );
+                ));
             }
+            default -> throw new InvalidShapeException();
         }
-        return new Tetromino(arr);
+        return null;
     }
 }
