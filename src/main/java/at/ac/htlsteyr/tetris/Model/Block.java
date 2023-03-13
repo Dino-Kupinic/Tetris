5f586232d675f9a197a2b6a96e433671979221e1
Neuery17Alt <jangerer@htl-steyr.ac.at>
2023-03-13 11:30:25 +0100

    Update Block, add attributes and initialiser with Getter and Setter and Constructor

--- src/main/java/at/ac/htlsteyr/tetris/Model/Block.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Block.java
@@ -1,10 +1,39 @@
 package at.ac.htlsteyr.tetris.Model;
 
+import javafx.scene.paint.Color;
+
 public class Block {
-    Point a;
+    Point blockPoints;
 
+    Color blockColor;
 
-    public void a() {
-        int rows = GridSize.rows;
+    int x,y;
+
+    public Block(Point p, Color c) {
+        blockColor = c;
+        blockPoints = p;
     }
+
+    public void initCoords () {
+        y = GridSize.rows;
+        x = GridSize.columns;
+    }
+
+    public Point getBlockcoords() {
+        return blockPoints;
+    }
+
+    public Color getBlockColor() {
+        return blockColor;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+
 }
