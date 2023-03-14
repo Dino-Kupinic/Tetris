b96a25a37667e90c41c96c3aadc26955a26a07d4
Neuery17Alt <jangerer@htl-steyr.ac.at>
2023-03-14 14:34:01 +0100

    Update Grid, added extension to GridPane, fixed grid generating

--- src/main/java/at/ac/htlsteyr/tetris/Model/Grid.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Grid.java
@@ -1,30 +1,31 @@
 package at.ac.htlsteyr.tetris.Model;
-import at.ac.htlsteyr.tetris.Controller.MainController;
+
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 
-public class Grid {
+public class Grid extends GridPane {
     private Field[][] grid;
+
     Pane root;
 
-    public Grid (Pane root) {
+    public Grid(Pane root) {
         this.root = root;
     }
 
-    public void generateGrid () {
+    public Pane generateGrid() {
         int cols = GridSize.columns;
         int rows = GridSize.rows;
         grid = new Field[cols][rows];
 
 
-
         for (int x = 0; x < cols; x++) {
             for (int y = 0; y < rows; y++) {
-                Field field = new Field(cols, rows);
+                Field field = new Field(x, y);
                 grid[x][y] = field;
                 root.getChildren().add(field);
             }
         }
-
+        return root;
     }
 
 
