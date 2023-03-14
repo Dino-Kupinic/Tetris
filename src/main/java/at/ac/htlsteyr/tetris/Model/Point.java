bfd175dfc7a5b326e5ff8111ac77690b0c89c003
dkupinic <dkupinic@htl-steyr.ac.at>
2023-03-14 13:56:12 +0100

    changed point getter and setter to private

--- src/main/java/at/ac/htlsteyr/tetris/Model/Point.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Point.java
@@ -9,19 +9,19 @@ public class Point {
         this.y = y;
     }
 
-    public int getX() {
+    private int getX() {
         return x;
     }
 
-    public int getY() {
+    private int getY() {
         return y;
     }
 
-    public void setX(int x) {
+    private void setX(int x) {
         this.x = x;
     }
 
-    public void setY(int y) {
+    private void setY(int y) {
         this.y = y;
     }
 }
