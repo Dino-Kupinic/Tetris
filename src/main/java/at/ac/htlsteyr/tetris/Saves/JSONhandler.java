0c918260b23b8a466b5555a53686fe81487db5b5
dkupinic <dkupinic@htl-steyr.ac.at>
2023-03-20 13:30:59 +0100

    trying to include GSON in JavaFX

--- src/main/java/at/ac/htlsteyr/tetris/Saves/JSONhandler.java
+++ src/main/java/at/ac/htlsteyr/tetris/Saves/JSONhandler.java
@@ -1,4 +1,13 @@
 package at.ac.htlsteyr.tetris.Saves;
 
 public class JSONhandler {
+    Gson gson;
+
+    public JSONhandler {
+        gson = new GsonBuilder().setPrettyPrinting().create();
+    }
+
+    public void writeToJSON(String name, int highscore) {
+
+    }
 }
