955929af60f5401b457ec7b519de99607f3b58b4
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-03-20 20:41:23 +0100

    added gson

--- src/main/java/at/ac/htlsteyr/tetris/Saves/JSONhandler.java
+++ src/main/java/at/ac/htlsteyr/tetris/Saves/JSONhandler.java
@@ -4,25 +4,20 @@ package at.ac.htlsteyr.tetris.Saves;
 import at.ac.htlsteyr.tetris.Model.Player;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
 
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class JSONhandler {
-    Gson gson;
-
-    public JSONhandler() {
-        gson = new GsonBuilder().setPrettyPrinting().create();
-    }
+    private final Path PATH_TO_JSON = Paths.get("src/main/java/at/ac/htlsteyr/tetris/savefile/player.json");
+    private final Gson gson = new GsonBuilder().setPrettyPrinting().create();
 
     public void writeToJSON(String name, int highscore) {
-        String json = gson.toJson(new Player(name, highscore));
-        try {
-            FileWriter fileWriter = new FileWriter("src/main/java/at/ac/htlsteyr/tetris/savefile/player.json");
-            fileWriter.write(json);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        String json = gson.toJson(new Player(name, highscore), Player.class);
     }
 }
