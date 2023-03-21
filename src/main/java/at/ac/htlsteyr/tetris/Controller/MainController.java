6829c3e36d118944b0d5fe3d86f12b85b7b0ead6
Neuery17Alt <jangerer@htl-steyr.ac.at>
2023-03-21 13:39:37 +0100

    Update MainController , added method to create the content

--- src/main/java/at/ac/htlsteyr/tetris/Controller/MainController.java
+++ src/main/java/at/ac/htlsteyr/tetris/Controller/MainController.java
@@ -1,23 +1,43 @@
 package at.ac.htlsteyr.tetris.Controller;
 
-import at.ac.htlsteyr.tetris.Model.Grid;
+import at.ac.htlsteyr.tetris.Exceptions.InvalidShapeException;
+import at.ac.htlsteyr.tetris.Model.*;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+
+import java.util.ArrayList;
 
 public class MainController {
     @FXML
     AnchorPane anchorField;
 
     private Pane root = new Pane();
+    Grid grid;
 
 
     public void initialize() {
-
         root.setPrefSize(300, 600);
-        Grid grid = new Grid(root);
+        grid = new Grid(root);
         root = grid.generateGrid();
         anchorField.getChildren().add(root);
+
+        createContent();
+    }
+
+    public void createContent() {
+        int x, y;
+
+        Field[][] myGrid = grid.getGrid();
+
+        myGrid[1][0].getFieldNode().setFill(Color.BLUE);
+        myGrid[1][0] = new Field(1, 0);
+        myGrid[1][1].getFieldNode().setFill(Color.BLUE);
+        myGrid[1][1] = new Field(1, 0);
+        myGrid[1][2].getFieldNode().setFill(Color.BLUE);
+        myGrid[1][2] = new Field(1, 0);
+
     }
 
 
