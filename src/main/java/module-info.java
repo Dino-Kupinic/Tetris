4aecebea656552c440800538cfc01e6718b74265
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-03-26 18:58:32 +0200

    exposed the Model package to gson to serialize

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,7 +3,7 @@ module at.ac.htlsteyr.tetris {
     requires javafx.fxml;
     requires com.google.gson;
 
-
+    opens at.ac.htlsteyr.tetris.Model to com.google.gson;
     opens at.ac.htlsteyr.tetris to javafx.fxml;
     exports at.ac.htlsteyr.tetris;
     exports at.ac.htlsteyr.tetris.Controller;
