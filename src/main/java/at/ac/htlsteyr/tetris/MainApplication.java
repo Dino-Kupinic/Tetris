9a4c71c3865cc115ca29a991fd6b8daff8b2e715
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-03-26 18:57:39 +0200

    removed json test

--- src/main/java/at/ac/htlsteyr/tetris/MainApplication.java
+++ src/main/java/at/ac/htlsteyr/tetris/MainApplication.java
@@ -1,11 +1,10 @@
 package at.ac.htlsteyr.tetris;
 
+import at.ac.htlsteyr.tetris.Model.Player;
 import at.ac.htlsteyr.tetris.Saves.JSONhandler;
 import javafx.application.Application;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -22,9 +21,6 @@ public class MainApplication extends Application {
         stage.setResizable(false);
         stage.setScene(scene);
         stage.show();
-
-        JSONhandler jsoNhandler = new JSONhandler();
-        jsoNhandler.writeToJSON("Samc", 33);
     }
 
     public static void main(String[] args) {
