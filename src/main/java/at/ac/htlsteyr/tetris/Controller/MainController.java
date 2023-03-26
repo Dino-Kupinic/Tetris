afce6c09ebfed7d96d6e38fda29cb7240c2ceacf
Dino Kupinic <dkupinic@htl-steyr.ac.at>
2023-03-26 18:58:04 +0200

    moved createContent() to Game.java

--- src/main/java/at/ac/htlsteyr/tetris/Controller/MainController.java
+++ src/main/java/at/ac/htlsteyr/tetris/Controller/MainController.java
@@ -1,83 +1,27 @@
 package at.ac.htlsteyr.tetris.Controller;
 
+import at.ac.htlsteyr.tetris.Model.Game;
 import at.ac.htlsteyr.tetris.Model.Grid;
 import javafx.fxml.FXML;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
-import javafx.scene.paint.Color;
-
-import java.util.ArrayList;
 
 public class MainController {
     @FXML
-    AnchorPane anchorField;
-    private static Pane root = new Pane();
+    private AnchorPane anchorField;
+    private Pane root;
+    private Grid grid;
     private static MainController instance;
 
     public void initialize() {
-        Grid grid = new Grid(root);
-        grid.generateGrid();
+        root = new Pane();
         root.setPrefSize(300, 600);
         grid = new Grid(root);
         root = grid.generateGrid();
         anchorField.getChildren().add(root);
 
-        createContent();
-    }
-
-    public void createContent() {
-        Field[][] myGrid = grid.getGrid();
-
-        TetrominoFactory tetroFac = new TetrominoFactory();
-        try {
-            int MAX_NUM = 6;
-            int MIN_NUM = 0;
-            int randNumber = (int) (Math.random() * MAX_NUM + MIN_NUM);
-
-            Shapes shape = null;
-            switch (randNumber) {
-                case 0 -> {
-                    shape = Shapes.SHAPE_I;
-                }
-                case 1 -> {
-                    shape = Shapes.SHAPE_L;
-                }
-                case 2 -> {
-                    shape = Shapes.SHAPE_J;
-                }
-                case 3 -> {
-                    shape = Shapes.SHAPE_T;
-                }
-                case 4 -> {
-                    shape = Shapes.SHAPE_O;
-                }
-                case 5 -> {
-                    shape = Shapes.SHAPE_S;
-                }
-                case 6 -> {
-                    shape = Shapes.SHAPE_Z;
-                }
-            }
-
-            Tetromino tetro = tetroFac.createTetromino(shape);
-            int[][] tetroGrid = tetro.getTetroGrid();
-
-            for (int row = 0; row < tetroGrid.length; row++) {
-                for (int col = 0; col < tetroGrid[row].length; col++) {
-                    if (tetroGrid[row][col] == 1) {
-                        myGrid[row][col].getFieldNode().setFill(Color.BLUE);
-                        myGrid[row][col] = new Field(row, col);
-                    }
-                }
-            }
-
-
-
-        } catch (InvalidShapeException e) {
-            throw new RuntimeException(e);
-        }
-
-
+        Game game = new Game();
+        game.createContent(grid);
     }
 
     public MainController() {
