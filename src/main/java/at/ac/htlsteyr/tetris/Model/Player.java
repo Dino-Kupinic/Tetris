bd0b6618d8977655ec05e012882a852be4e2ab1b
dkupinic <dkupinic@htl-steyr.ac.at>
2023-03-27 12:50:34 +0200

    changed Player class to record

--- src/main/java/at/ac/htlsteyr/tetris/Model/Player.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Player.java
@@ -1,27 +1,4 @@
 package at.ac.htlsteyr.tetris.Model;
 
-public class Player {
-    private String name;
-    private int highscore;
+public record Player(String name, Integer highscore) {}
 
-    public Player(String name, int highscore) {
-        this.name = name;
-        this.highscore = highscore;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getHighscore() {
-        return highscore;
-    }
-
-    public void setHighscore(int highscore) {
-        this.highscore = highscore;
-    }
-}
