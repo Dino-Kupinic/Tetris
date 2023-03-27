a1a352f5b932caec66926109073a1ec795b6dbe2
dkupinic <dkupinic@htl-steyr.ac.at>
2023-03-27 13:29:11 +0200

    changed GridSize to class

--- src/main/java/at/ac/htlsteyr/tetris/Model/GridSize.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/GridSize.java
@@ -1,6 +1,6 @@
 package at.ac.htlsteyr.tetris.Model;
 
-public record GridSize() {
+public class GridSize {
     public static final int width = 300;
     public static final int height = 600;
     public static final int columns = 10;
