a0c82ea68cb3ee694b323c60d7321a0cf925fe32
dkupinic <dkupinic@htl-steyr.ac.at>
2023-03-27 13:36:24 +0200

    changed class to record

--- src/main/java/at/ac/htlsteyr/tetris/Model/Point.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Point.java
@@ -1,27 +1,3 @@
 package at.ac.htlsteyr.tetris.Model;
 
-public class Point {
-    private int x;
-    private int y;
-
-    public Point(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-}
+public record Point(int x, int y) {}
