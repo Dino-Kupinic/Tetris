67a1b503e729850cd0af10f601f8cc265c116db0
dkupinic <dkupinic@htl-steyr.ac.at>
2023-03-27 13:30:42 +0200

    override toString()

--- src/main/java/at/ac/htlsteyr/tetris/Model/Player.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Player.java
@@ -1,4 +1,9 @@
 package at.ac.htlsteyr.tetris.Model;
 
-public record Player(String name, Integer highscore) {}
+public record Player(String name, Integer highscore) {
+    @Override
+    public String toString() {
+        return "Player=[NAME: " + name + " | HIGHSCORE: " + highscore + "]";
+    }
+}
 
