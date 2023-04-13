9207913062682a0b9c91ef6212ba0af722303d8c
Neuery17Alt <jangerer@htl-steyr.ac.at>
2023-04-13 14:40:18 +0200

    Update Game, added Tetrominoheigt and width

--- src/main/java/at/ac/htlsteyr/tetris/Model/TetrominoFactory.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/TetrominoFactory.java
@@ -7,10 +7,13 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 public class TetrominoFactory {
+    int tetroHeight = 0;
+    int tetroWidth = 0;
     public Tetromino createTetromino(TetrominoShapes shape) throws InvalidShapeException {
         switch (shape) {
             case SHAPE_I -> {
                 Color col = Color.BLUE;
+                tetroHeight=4;
                 return new Tetromino(new ArrayList<>(Arrays.asList(
                         new Block(new Point(1, 0), col),
                         new Block(new Point(1, 1), col),
@@ -20,6 +23,7 @@ public class TetrominoFactory {
             }
             case SHAPE_L -> {
                 Color col = Color.ORANGE;
+                tetroHeight=3;
                 return new Tetromino(new ArrayList<>(Arrays.asList(
                         new Block(new Point(1, 1), col),
                         new Block(new Point(1, 2), col),
@@ -29,6 +33,7 @@ public class TetrominoFactory {
             }
             case SHAPE_J -> {
                 Color col = Color.DARKBLUE;
+                tetroHeight=3;
                 return new Tetromino(new ArrayList<>(Arrays.asList(
                         new Block(new Point(1, 1), col),
                         new Block(new Point(1, 2), col),
@@ -38,6 +43,7 @@ public class TetrominoFactory {
             }
             case SHAPE_T -> {
                 Color col = Color.PURPLE;
+                tetroHeight=2;
                 return new Tetromino(new ArrayList<>(Arrays.asList(
                         new Block(new Point(1, 2), col),
                         new Block(new Point(1, 3), col),
@@ -47,6 +53,7 @@ public class TetrominoFactory {
             }
             case SHAPE_O -> {
                 Color col = Color.YELLOW;
+                tetroHeight=2;
                 return new Tetromino(new ArrayList<>(Arrays.asList(
                         new Block(new Point(1, 2), col),
                         new Block(new Point(0, 2), col),
@@ -56,6 +63,7 @@ public class TetrominoFactory {
             }
             case SHAPE_S -> {
                 Color col = Color.GREEN;
+                tetroHeight=2;
                 return new Tetromino(new ArrayList<>(Arrays.asList(
                         new Block(new Point(0, 3), col),
                         new Block(new Point(1, 3), col),
@@ -65,6 +73,7 @@ public class TetrominoFactory {
             }
             case SHAPE_Z -> {
                 Color col = Color.RED;
+                tetroHeight=2;
                 return new Tetromino(new ArrayList<>(Arrays.asList(
                         new Block(new Point(0, 2), col),
                         new Block(new Point(1, 2), col),
