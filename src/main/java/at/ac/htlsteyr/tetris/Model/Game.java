29a784e7af7f0a0f5abe837cc9c903e6ecc1ce68
Neuery17Alt <jangerer@htl-steyr.ac.at>
2023-04-13 14:39:13 +0200

    Update Game, almost fixed Tetrominos falling down, tried fixing "I" Tetromino

--- src/main/java/at/ac/htlsteyr/tetris/Model/Game.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Game.java
@@ -57,22 +57,53 @@ public class Game {
                     int[][] tetroGrid = currentTetro.getTetroGrid();
                     ArrayList<Block> blocks = currentTetromino.getTetroBlocks();
                     Color color = blocks.get(0).blockColor;
-
+                    int zaehler=0;
                     // move tetro
                     for (int row = 0; row < tetroGrid.length; row++) {
                         for (int col = 0; col < tetroGrid[row].length; col++) {
+                            if (tetroFactory.tetroHeight == 4) {
+                                if (zaehler == 0) {
+                                    yOffset=1;
+                                    zaehler = zaehler+1;
+                                }
+                                // tetroGrid
+                                if (tetroGrid[row][col] == 0) {
+                                    gameGrid[row][col + yOffset].getFieldNode().setFill(Color.GRAY);
+                                }
 
-                            // tetroGrid
-                            if (tetroGrid[row][col] == 0) {
-                                gameGrid[row][col + yOffset].getFieldNode().setFill(Color.GRAY);
+                                // tetro
+                                if (tetroGrid[row][col] == 1) {
+                                    gameGrid[row][col + yOffset].getFieldNode().setFill(color);
+                                    gameGrid[row][col + yOffset].setContainsBlock(true);
+                                    gameGrid[row][col + yOffset].updateDebugText();
+                                }
+                            } else {
+                                // tetroGrid
+                                if (tetroGrid[row][col] == 0) {
+                                    gameGrid[row][col + yOffset].getFieldNode().setFill(Color.GRAY);
+                                }
+
+                                // tetro
+                                if (tetroGrid[row][col] == 1) {
+                                    gameGrid[row][col + yOffset].getFieldNode().setFill(color);
+                                    gameGrid[row][col + yOffset].setContainsBlock(true);
+                                    gameGrid[row][col + yOffset].updateDebugText();
+                                }
+
                             }
 
-                            // tetro
-                            if (tetroGrid[row][col] == 1) {
-                                gameGrid[row][col + yOffset].getFieldNode().setFill(color);
-                                gameGrid[row][col + yOffset].setContainsBlock(true);
-                                gameGrid[row][col + yOffset].updateDebugText();
+                            for (int checkRow = 0; checkRow < gameGrid.length; checkRow++) {
+                                for (int checkCol = 0; checkCol < gameGrid[checkRow].length; checkCol++) {
+                                    if (gameGrid[checkRow][checkCol].getFieldNodeColor().equals(Color.GRAY)){
+                                        gameGrid[checkRow][checkCol].getFieldNode().setFill(Color.WHITE);
+                                        gameGrid[checkRow][checkCol].setContainsBlock(false);
+                                        gameGrid[checkRow][checkCol].updateDebugText();
+                                    }
+                                }
+
                             }
+
+
                         }
                     }
                     xOffset++;
