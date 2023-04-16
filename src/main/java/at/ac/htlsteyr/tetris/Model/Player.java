02b511c5e971d19bdb2a14ef1f44fbcf29d913c3
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-16 15:46:05 +0200

    changed back to class due to json

--- src/main/java/at/ac/htlsteyr/tetris/Model/Player.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Player.java
@@ -1,6 +1,30 @@
 package at.ac.htlsteyr.tetris.Model;
 
-public record Player(String name, Integer highscore) {
+public class Player {
+    private String name;
+    private Integer highscore;
+
+    public Player(String name, Integer highscore) {
+        this.name = name;
+        this.highscore = highscore;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getHighscore() {
+        return highscore;
+    }
+
+    public void setHighscore(Integer highscore) {
+        this.highscore = highscore;
+    }
+
     @Override
     public String toString() {
         return "Player=[NAME: " + name + " | HIGHSCORE: " + highscore + "]";
