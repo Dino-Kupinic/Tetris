356059eaa9a74d5a8b3a3ecdf5f006f38300c082
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-16 15:48:12 +0200

    added getter and setter for gamemode property

--- src/main/java/at/ac/htlsteyr/tetris/Model/Game.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Game.java
@@ -8,14 +8,40 @@ import java.util.ArrayList;
 import java.util.Objects;
 
 public class Game {
-    Field[][] gameGrid;
-    TetrominoFactory tetroFactory;
-    Tetromino currentTetromino;
+    private static Gamemode gamemode;
+    private Field[][] gameGrid;
+    private TetrominoFactory tetroFactory;
+    private Tetromino currentTetromino;
 
-    public Game(Grid grid) {
-        gameGrid = grid.getGrid();
-        tetroFactory = new TetrominoFactory();
-        currentTetromino = null;
+    public Game(Grid grid, Gamemode mode) {
+        Game.gamemode = mode;
+        this.gameGrid = grid.getGrid();
+        this.tetroFactory = new TetrominoFactory();
+        this.currentTetromino = null;
+    }
+
+    public Field[][] getGameGrid() {
+        return gameGrid;
+    }
+
+    public void setGameGrid(Field[][] gameGrid) {
+        this.gameGrid = gameGrid;
+    }
+
+    public TetrominoFactory getTetroFactory() {
+        return tetroFactory;
+    }
+
+    public void setTetroFactory(TetrominoFactory tetroFactory) {
+        this.tetroFactory = tetroFactory;
+    }
+
+    public Tetromino getCurrentTetromino() {
+        return currentTetromino;
+    }
+
+    public void setCurrentTetromino(Tetromino currentTetromino) {
+        this.currentTetromino = currentTetromino;
     }
 
     public void createTetromino() {
@@ -57,14 +83,14 @@ public class Game {
                     int[][] tetroGrid = currentTetro.getTetroGrid();
                     ArrayList<Block> blocks = currentTetromino.getTetroBlocks();
                     Color color = blocks.get(0).blockColor;
-                    int zaehler=0;
+                    int zaehler = 0;
                     // move tetro
                     for (int row = 0; row < tetroGrid.length; row++) {
                         for (int col = 0; col < tetroGrid[row].length; col++) {
                             if (tetroFactory.tetroHeight == 4) {
                                 if (zaehler == 0) {
-                                    yOffset=1;
-                                    zaehler = zaehler+1;
+                                    yOffset = 1;
+                                    zaehler = zaehler + 1;
                                 }
                                 // tetroGrid
                                 if (tetroGrid[row][col] == 0) {
@@ -94,7 +120,7 @@ public class Game {
 
                             for (int checkRow = 0; checkRow < gameGrid.length; checkRow++) {
                                 for (int checkCol = 0; checkCol < gameGrid[checkRow].length; checkCol++) {
-                                    if (gameGrid[checkRow][checkCol].getFieldNodeColor().equals(Color.GRAY)){
+                                    if (gameGrid[checkRow][checkCol].getFieldNodeColor().equals(Color.GRAY)) {
                                         gameGrid[checkRow][checkCol].getFieldNode().setFill(Color.WHITE);
                                         gameGrid[checkRow][checkCol].setContainsBlock(false);
                                         gameGrid[checkRow][checkCol].updateDebugText();
@@ -102,8 +128,6 @@ public class Game {
                                 }
 
                             }
-
-
                         }
                     }
                     xOffset++;
