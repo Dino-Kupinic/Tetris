4966e1e2aae2961dbaecbda8f0981e79fbecfaae
Neuery17Alt <jangerer@htl-steyr.ac.at>
2023-04-17 10:54:37 +0200

    Update Tetromin, tried fixing moving block to right, left and fastdrop

--- src/main/java/at/ac/htlsteyr/tetris/Model/Tetromino.java
+++ src/main/java/at/ac/htlsteyr/tetris/Model/Tetromino.java
@@ -1,5 +1,7 @@
 package at.ac.htlsteyr.tetris.Model;
 
+import javafx.scene.paint.Color;
+
 import java.util.ArrayList;
 
 public class Tetromino {
@@ -11,6 +13,9 @@ public class Tetromino {
             {0, 0, 0, 0},
             {0, 0, 0, 0}
     };
+
+    Field[][] gameGrid;
+    Tetromino currentTetromino = null;
 
     public Tetromino(ArrayList<Block> blockList) {
         this.TetrominoBlockList = blockList;
@@ -29,14 +34,18 @@ public class Tetromino {
         return TetrominoBlockList;
     }
 
-    public void rotate(TetrominoMovements move) {
+    public void rotate(TetrominoMovements move, Grid grid, int xOffset) {
+        gameGrid = grid.getGrid();
+        ArrayList<Block> blocks = currentTetromino.getTetroBlocks();
+        Color color = blocks.get(0).blockColor;
         switch (move) {
             case RIGHT -> {
-                for (int i = 0; i < tetroGrid.length; i++) {
-                    for (int j = 0; j < tetroGrid[0].length; j++) {
-                        if (tetroGrid[i][j] == 1) {
-                            tetroGrid[i][j] = 0;
-                            tetroGrid[i][j + 1] = 1;
+                for (int row = 0; row < tetroGrid.length; row++) {
+                    for (int col = 0; col < tetroGrid[0].length; col++) {
+                        if (tetroGrid[row][col] == 1) {
+                            gameGrid[row + xOffset][col].getFieldNode().setFill(color);
+                            gameGrid[row + xOffset][col].setContainsBlock(true);
+                            gameGrid[row + xOffset][col].updateDebugText();
                         }
                     }
                 }
