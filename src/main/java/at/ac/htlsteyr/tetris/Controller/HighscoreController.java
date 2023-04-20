40c598288be9c93b5ebb51d049fc2290ca49e2e0
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-20 20:11:28 +0200

    added contiue and player name, also added function for when the player clicks the continue button

--- src/main/java/at/ac/htlsteyr/tetris/Controller/HighscoreController.java
+++ src/main/java/at/ac/htlsteyr/tetris/Controller/HighscoreController.java
@@ -1,4 +1,20 @@
 package at.ac.htlsteyr.tetris.Controller;
 
+import at.ac.htlsteyr.tetris.Saves.JSONhandler;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+import java.io.IOException;
+
 public class HighscoreController {
+    public Button continueButton;
+    public TextField playerNameInput;
+
+    @FXML
+    public void onContinueButtonClicked() throws IOException {
+        JSONhandler handler = new JSONhandler();
+        handler.checkIfSaveJSONisValid();
+        handler.writePlayerToSaveJSON(playerNameInput.getText(), Integer.parseInt(MainController.getInstance().score.getText()));
+    }
 }
