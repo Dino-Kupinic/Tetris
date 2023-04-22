77f9480308d76e74b59146c6d8a223e42ca0c0b8
Dino-Kupinic <dkupinic@htl-steyr.ac.at>
2023-04-22 16:51:30 +0200

    minor changes

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -7,5 +7,6 @@ module at.ac.htlsteyr.tetris {
     opens at.ac.htlsteyr.tetris to javafx.fxml;
     exports at.ac.htlsteyr.tetris;
     exports at.ac.htlsteyr.tetris.Controller;
+    exports at.ac.htlsteyr.tetris.Model;
     opens at.ac.htlsteyr.tetris.Controller to javafx.fxml;
 }
